0d454a2772d58c64eb2e0d3961e14ad95eeaa575
skinsler <simonkinsler@gmail.com>
2019-04-24 18:35:32 -0500

    changing local repository

---
+++ assets/javascript/game.js
@@ -0,0 +1,38 @@
+// //Constants
+
+// const MAX_GUESSES = 8;
+
+// //Global Variables
+// var dictionary = ["Aladdin", "Inception", "Shrek", "Psycho", "Jaws"]
+// var guessesRemaining;
+// var alreadyGuessed = [];
+// var wins = 0;
+
+
+// var content = document.createElement("div");
+// content.setAttribute("id", "content");
+// document.body.appendChild(content);
+  
+
+
+// function getKey(e) {
+//   return e;
+// }
+
+// function removeWelcome () {
+//   document.getElementById('welcome-text').remove();
+// }
+
+// removeWelcome();
+
+
+var element = document.getElementById("welcome");
+console.log (element.innerHTML);
+
+// function startGame() {
+//   document.addEventListener('keydown', removeWelcome);
+  
+// }
+
+
+
