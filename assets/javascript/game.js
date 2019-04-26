d325c4e3a15ea2e47f529afd767498e9e5f12bc5
skinsler <simonkinsler@gmail.com>
2019-04-25 20:51:54 -0500

    added keydown event and game start logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,13 @@
-// //Constants
+//Constants
 
-// const MAX_GUESSES = 8;
+const MAX_GUESSES = 8;
 
-// //Global Variables
-// var dictionary = ["Aladdin", "Inception", "Shrek", "Psycho", "Jaws"]
-// var guessesRemaining;
-// var alreadyGuessed = [];
-// var wins = 0;
+//Global Variables
+var dictionary = ["Aladdin", "Inception", "Shrek", "Psycho", "Jaws"]
+var guessesRemaining;
+var alreadyGuessed = [];
+var wins = 0;
+var isGameStarted = false;
 
 
 // var content = document.createElement("div");
@@ -15,24 +16,24 @@
   
 
 
-// function getKey(e) {
-//   return e;
-// }
-
-// function removeWelcome () {
-//   document.getElementById('welcome-text').remove();
-// }
-
-// removeWelcome();
 
 
-var element = document.getElementById("welcome");
-console.log (element.innerHTML);
-
-// function startGame() {
-//   document.addEventListener('keydown', removeWelcome);
-  
-// }
+function removeWelcome () {
+  document.getElementById('welcome-message').remove();
+}
 
 
+let welcomeMessageElement = document.getElementById("welcome-message");
+console.log (welcomeMessageElement.innerHTML);
+
+
+document.onkeyup = function(event) {
+    if (!isGameStarted) {
+        removeWelcome();
+        var word = dictionary[Math.floor(Math.random()*dictionary.length)];
+        console.log(word);
+        isGameStarted = true;
+    }
+    console.log(event.key);
+}
 
